68cf799bb46d802ef9b778575f160d56c39bbd77
ShristiA <shristiamatya3@gmail.com>
2018-11-27 09:41:28 -0500

    without the aries logo

--- src/main/java/com/careydevelopment/highchartsdemo/Security/SecurityConfiguration.java
+++ src/main/java/com/careydevelopment/highchartsdemo/Security/SecurityConfiguration.java
@@ -34,7 +34,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/","/h2-console/**","/register/**","/assets/**").permitAll()
+                .antMatchers("/","/h2-console/**","/register/**","/static/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/login").permitAll()
